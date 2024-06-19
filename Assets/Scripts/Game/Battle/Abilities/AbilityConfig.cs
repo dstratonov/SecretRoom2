bb5282d706c92b1d91561716b33720b45c1e5b9b
odemeshko <urban.fl4m3@gmail.com>
2024-06-19 18:38:42 +0300

    [DEV-0] Added ability ID config

---
+++ Assets/Scripts/Game/Battle/Abilities/AbilityConfig.cs
@@ -0,0 +1,12 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Game.Battle.Abilities
+{
+    [CreateAssetMenu(fileName = nameof(AbilityConfig), menuName = "Battle/Configs/Ability")]
+    public class AbilityConfig : SerializedScriptableObject
+    {
+        [AbilityId]
+        public string id;
+    }
+}
---
+++ Assets/Scripts/Game/Battle/Abilities/AbilityIdsConfig.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Game.Battle.Abilities
+{
+    public class AbilityIdsConfig : ScriptableObject
+    {
+        public string[] ids;
+    }
+}
