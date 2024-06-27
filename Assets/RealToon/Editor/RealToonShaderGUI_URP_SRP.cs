684e82c33a1084e3aa959632cdcbf07b9bd43b88
odemeshko <urban.fl4m3@gmail.com>
2024-06-27 22:34:09 +0300

    [DEV-0] Fixed unassigned warning

--- Assets/RealToon/Editor/RealToonShaderGUI_URP_SRP.cs
+++ Assets/RealToon/Editor/RealToonShaderGUI_URP_SRP.cs
@@ -64,7 +64,9 @@ static bool twofourfive_target = false;
 static string twofourfive_target_string = "Change shader compilation target to 4.5";
 
 static bool dots_lbs_cd = false;
+#pragma warning disable 0414
 static string dots_lbs_cd_string = "DOTS|HR - Use Compute Deformation";
+#pragma warning restore
 
         #endregion
 
